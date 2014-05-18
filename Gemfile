1cc981791c53f72845a45f39d2a3ce83003891d8
andrewoji <andrewoji@gmail.com>
2014-05-18 17:47:32 +0100

    formtting and counting now separate responsibilities

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem 'rest-client', '1.6.7'
+gem 'thor', '0.19.1'
+gem 'json', '1.8.1'
+
+group :test do
+  gem 'rspec', '2.14.1'
+  gem 'webmock', '1.17.4'
+end
--- lib/github_repos_analyser.rb
+++ lib/github_repos_analyser.rb
@@ -1,38 +1,42 @@
 require 'restclient'
 require 'json'
 class GitHubReposAnalyser
+
   def initializer
     @user_name = ""
   end
+
   def get_favourite_language_by_user(user_name)
     @user_name = user_name
     json = repos_to_json
     counts = json_to_language_counts json
-    format_language_counts counts
+    languages = favourites counts
+    format languages
   end
+
   private
+
     def repos_to_json
       url = "https://api.github.com/users/#{@user_name}/repos"
-      response = RestClient.get(url,{accept: 'application/vnd.github.beta+json'})
-      json = JSON.parse(response)
+      JSON.parse RestClient.get(url,{accept: 'application/vnd.github.beta+json'})
     end
     
     def json_to_language_counts(json)
       language_counts = json.inject(Hash.new(0)) do |hsh, repo|
-        hsh[repo['language']] += 1 unless repo['language'].nil?
+        repo['language'].nil? || hsh[repo['language']] += 1
         hsh
       end
     end
 
-    def format_language_counts(language_counts)
-      return "#{@user_name} has no favourite programming language defined\." if language_counts.empty?
-      language = language_counts
-                  .select {|k,v| v == language_counts.values.max}
-                  .keys
-                  .join(',')
+    def favourites(language_counts)
+      language_counts.select {|k,v| v == language_counts.values.max}.keys
+    end
+
+    def format(favourites)
+      return "#{@user_name} has no favourite programming language defined\." if favourites.empty?
       
-      language.include?(',') ?
-        "#{language} are the favourite programming languages of user #{@user_name}" : 
-        "#{language} is the favourite programming language of user #{@user_name}"
+      favourites.one? ?
+        "#{favourites.first} is the favourite programming language of user #{@user_name}" :
+        "#{favourites.join(',')} are the favourite programming languages of user #{@user_name}" 
     end
 end
