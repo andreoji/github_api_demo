8aea106c6964b3b059a0486a696946209d7b76ad
andrewoji <andrewoji@gmail.com>
2014-06-06 15:13:37 +0100

    silly file added

--- lib/github_repos_analyser.rb
+++ lib/github_repos_analyser.rb
@@ -1,42 +1,38 @@
-require 'restclient'
+require 'octokit'
 require 'json'
 class GitHubReposAnalyser
 
-  def initializer
+  def initialize
     @user_name = ""
   end
 
   def get_favourite_language_by_user(user_name)
     @user_name = user_name
-    json = repos_to_json
-    counts = json_to_language_counts json
-    languages = favourites counts
-    format languages
+    Octokit.auto_paginate = true
+    languages = favourite_languages(Octokit.repositories(@user_name))
+    format languages   
   end
 
   private
-
-    def repos_to_json
-      url = "https://api.github.com/users/#{@user_name}/repos"
-      JSON.parse RestClient.get(url,{accept: 'application/vnd.github.beta+json'})
-    end
     
-    def json_to_language_counts(json)
-      language_counts = json.inject(Hash.new(0)) do |hsh, repo|
-        repo['language'].nil? || hsh[repo['language']] += 1
-        hsh
-      end
+  def language_counts(repos)
+    repos.inject(Hash.new(0)) do |hsh, repo|
+      hsh[repo.language] += 1 if repo.language
+      hsh
     end
+  end
 
-    def favourites(language_counts)
-      language_counts.select {|k,v| v == language_counts.values.max}.keys
-    end
+  def favourite_languages(repos)
+    counts = language_counts(repos)
+    max_count = counts.values.max
+    counts.select{|k,v| v == max_count}.keys
+  end
 
-    def format(favourites)
-      return "#{@user_name} has no favourite programming language defined\." if favourites.empty?
-      
-      favourites.one? ?
-        "#{favourites.first} is the favourite programming language of user #{@user_name}" :
-        "#{favourites.join(',')} are the favourite programming languages of user #{@user_name}" 
-    end
+  def format(favourites)
+    return "#{@user_name} has no favourite programming language defined\." if favourites.empty?
+    
+    favourites.size == 1 ?
+      "#{favourites.first} is the favourite programming language of user #{@user_name}" :
+      "#{favourites.join(',')} are the favourite programming languages of user #{@user_name}" 
+  end
 end
--- spec/github_repos_analyser_spec.rb
+++ spec/github_repos_analyser_spec.rb
@@ -1,20 +1,26 @@
 require 'webmock/rspec'
 require 'github_repos_analyser'
 include WebMock::API
-
+include RSpec::Matchers
 describe GitHubReposAnalyser do
-  let (:repos_analyser) { GitHubReposAnalyser.new}
   let (:valid_user_name) {"steveklabnik"}
+
   let (:headers) {
     { 
-      'Accept'          => 'application/vnd.github.beta+json',
-      'Accept-Encoding' => 'gzip, deflate', 
-      'User-Agent'      => 'Ruby'
+      'Accept'          => 'application/vnd.github.v3+json',
+      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 
+      'User-Agent'      => 'Octokit Ruby Gem 3.1.0'
+    }
+  }
+
+  let (:response_headers){
+    {
+      content_type: 'application/json'
     }
   }
   
   def uri_of_users_repos(user_name)
-    "https://api.github.com/users/#{user_name}/repos"
+    %r[^https://api.github.com/users/#{user_name}/repos]
   end
   
   context 'given steveklabnik with 3 Ruby, 1 JavaScript and 1 Shell repos' do
@@ -25,25 +31,27 @@ describe GitHubReposAnalyser do
         {\"language\":\"JavaScript\"},
         {\"language\":\"Shell\"}]"
     }
+
     before do
-      stub_request(:get, uri_of_users_repos("steveklabnik"))
+      stub_request(:get, uri_of_users_repos(valid_user_name))
         .with(headers: headers)
-        .to_return(status: 200, body: response_body)
+        .to_return(status: 200, body: response_body, headers: response_headers)
     end
+
     it 'should make a GET to the GitHub api' do
-      repos_analyser.get_favourite_language_by_user(valid_user_name)
-      WebMock
-        .should have_requested(:get, uri_of_users_repos(valid_user_name))
-        .with(headers: headers)
+      subject.get_favourite_language_by_user(valid_user_name)
+      expect(WebMock).to have_requested(:get, uri_of_users_repos(valid_user_name)).with(headers: headers)
     end
+
     it 'should find Steves favourite language to be Ruby' do
-      repos_analyser
+      expect(subject
         .get_favourite_language_by_user(valid_user_name)
-        .should == "Ruby is the favourite programming language of user steveklabnik"
+      ).to eq "Ruby is the favourite programming language of user steveklabnik"
     end
   end
 
   context 'given steveklabnik with 3 Ruby, 3 JavaScript and 1 Shell repos' do
+
     let (:response_body) {
       "[{\"language\":\"Ruby\"},
         {\"language\":\"Ruby\"},
@@ -53,20 +61,23 @@ describe GitHubReposAnalyser do
         {\"language\":\"JavaScript\"},
         {\"language\":\"Shell\"}]"
     }
+
     before do
-      stub_request(:get, uri_of_users_repos("steveklabnik"))
+      stub_request(:get, uri_of_users_repos(valid_user_name))
         .with(headers: headers)
-        .to_return(status: 200, body: response_body)
+        .to_return(status: 200, body: response_body, headers: response_headers)
     end
+
     it 'should find Steves favourite languages to be Ruby,JavaScript' do
-      repos_analyser
+      expect(subject
         .get_favourite_language_by_user(valid_user_name)
-        .should == "Ruby,JavaScript are the favourite programming languages of user steveklabnik"
+      ).to eql "Ruby,JavaScript are the favourite programming languages of user steveklabnik"
     end
   end
 
   context 'given shell_user with 3 null language, 2 Shell and 1 Ruby repos' do
     shell_user = "shell_user"
+
     let (:response_body) {
       "[{\"language\":\"Shell\"},
         {\"language\":null},
@@ -75,20 +86,23 @@ describe GitHubReposAnalyser do
         {\"language\":null},
         {\"language\":\"Ruby\"}]"
     }
+
     before do
       stub_request(:get, uri_of_users_repos(shell_user))
         .with(headers: headers)
-        .to_return(status: 200, body: response_body)
+        .to_return(status: 200, body: response_body, headers: response_headers)
     end
+
     it 'should find shell_users favourite language is Shell' do
-      repos_analyser
+      expect(subject
         .get_favourite_language_by_user(shell_user)
-        .should == "Shell is the favourite programming language of user shell_user"
+      ).to eql "Shell is the favourite programming language of user shell_user"
     end
   end
 
   context 'given null_lanaguage_user with 6 null language repos' do
     null_language_user = "null_language_user"
+
     let (:response_body) {
       "[{\"language\":null},
         {\"language\":null},
@@ -97,42 +111,50 @@ describe GitHubReposAnalyser do
         {\"language\":null},
         {\"language\":null}]"
     }
+
     before do
       stub_request(:get, uri_of_users_repos(null_language_user))
         .with(headers: headers)
-        .to_return(status: 200, body: response_body)
+        .to_return(status: 200, body: response_body, headers: response_headers)
     end
+
     it 'should find there is no favourite programming language' do
-      repos_analyser
+      expect(subject
         .get_favourite_language_by_user(null_language_user)
-        .should == "null_language_user has no favourite programming language defined."
+      ).to eql "null_language_user has no favourite programming language defined."
     end
   end
 
   context 'given a user without any repos' do
     empty_user = "empty_user"
     let (:response_body) {"[]"}
+
     before do 
       stub_request(:get, uri_of_users_repos(empty_user))
         .with(headers: headers)
-        .to_return(status:200, body: response_body)
+        .to_return(status:200, body: response_body, headers: response_headers)
     end
+
     it 'should find there is no favourite programming language' do
-      repos_analyser
+      expect(subject
         .get_favourite_language_by_user("#{empty_user}")
-        .should == "empty_user has no favourite programming language defined."
+      ).to eql "empty_user has no favourite programming language defined."
     end
   end
 
   context 'a user that does not exist' do
-    it 'should raise a ResourceNotFound exception' do
+
+    it 'should raise a Octokit::NotFound exception' do
       invalid_user_name = "stevexklabnik"
-      stub_request(:get, "https://api.github.com/users/#{invalid_user_name}/repos")
-        .to_raise(RestClient::ResourceNotFound)
+      stub_request(:get, uri_of_users_repos(invalid_user_name))
+        .with(headers: headers)
+        .to_return(status: 404)
+
       expect {
-        repos_analyser
+        subject
           .get_favourite_language_by_user(invalid_user_name)
-      }.to raise_error(RestClient::ResourceNotFound)
+      }.to raise_error(Octokit::NotFound)
+
       WebMock
         .should have_requested(:get, uri_of_users_repos(invalid_user_name))
         .with(headers: headers)      
